3b4b4cc13cb51cbd85d902351cb678e0b15b2fe5
Arya Agung Wicaksono <130280173+Agwexy@users.noreply.github.com>
2023-09-30 07:17:00 +0700

    Update App2.cpp

--- App2.cpp
+++ App2.cpp
@@ -1,41 +1,41 @@
 #include <iostream>
+using namespace std;
 
 // Main Program
 
 int main() {
     // Variabel
     int angka = 10;
-    double nilai = 3.14;
     char karakter = 'A';
     bool benar = true;
 
     // Tipe data
     int bulan = 9;
-    std::string namaBulan = "September";
+    string namaBulan = "September";
 
     // For loop
     for (int i = 1; i <= 5; i++) {
-        std::cout << "Iterasi ke-" << i << std::endl;
+        cout << "Iterasi ke-" << i << std::endl;
     }
 
     // If statement
     if (angka > 5) {
-        std::cout << "Angka lebih besar dari 5." << std::endl;
+        cout << "Angka lebih besar dari 5" << std::endl;
     } else {
-        std::cout << "Angka tidak lebih besar dari 5." << std::endl;
+        cout << "Angka tidak lebih besar dari 5" << std::endl;
     }
 
     // While loop
     int j = 1;
     while (j <= 3) {
-        std::cout << "Perulangan while ke-" << j << std::endl;
+        cout << "Perulangan while ke-" << j << std::endl;
         j++;
     }
 
     // Do-while loop
     int k = 1;
     do {
-        std::cout << "Perulangan do-while ke-" << k << std::endl;
+        cout << "Perulangan do-while ke-" << k << std::endl;
         k++;
     } while (k <= 3);
 
@@ -47,12 +47,12 @@ int main() {
 
     // Input
     int inputAngka;
-    std::cout << "Masukkan sebuah angka: ";
-    std::cin >> inputAngka;
-    std::cout << "Anda memasukkan angka: " << inputAngka << std::endl;
+    cout << "Masukkan sebuah angka: ";
+    cin >> inputAngka;
+    cout << "Angka yang dimasukan: " << inputAngka << std::endl;
 
     // Output
-    std::cout << "Halo, dunia!" << std::endl;
+    cout << "Halo, dunia!" << std::endl;
 
     // Comment
     // Ini adalah komentar satu baris
